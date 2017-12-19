5e6574099bc457073d5cc559e61ab542531c9078
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-12-19 17:18:19 +0700

    Added Arduino starting code

---
+++ NodeMCU/iLight/iLight.ino
@@ -0,0 +1,97 @@
+#include<SoftwareSerial.h>
+
+#include <MicroGear.h>
+#include <ESP8266WiFi.h>
+
+#define APPID   "iLight"
+#define KEY     "Q3XUy22q0cgxB9R"
+#define SECRET  "LgJmdco46P1GzPEslEhJBQ7il"
+
+const char* ssid     = "Aphires";
+const char* password = "AllHearsMe";
+
+#define ALIAS   "iLight_NodeMCU"
+#define TargetWeb "HTML_web_test"
+
+SoftwareSerial STMSerial(5, 4); // D1, D2
+
+WiFiClient client;
+MicroGear microgear(client);
+
+int test_count;
+
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) 
+{
+  Serial.print("Incoming message --> ");
+  msg[msglen] = '\0';
+  Serial.println((char *) msg);
+  STMSerial.print('[');
+  STMSerial.print((char *) msg);
+  STMSerial.print(']');
+}
+
+
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) 
+{
+  Serial.println("Connected to NETPIE...");
+  microgear.setAlias(ALIAS);
+}
+
+void setup() 
+{
+   /* Event listener */
+  microgear.on(MESSAGE, onMsghandler);
+  microgear.on(CONNECTED, onConnected);
+  
+  Serial.begin(115200);
+  Serial.println("Starting...");
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(250);
+    Serial.print(".");
+  }
+  
+  Serial.print("Connected to WiFi: ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  microgear.init(KEY,SECRET,ALIAS);
+  microgear.connect(APPID);
+  
+  test_count = 0;
+}
+
+void loop() 
+{
+  if (microgear.connected())
+  {
+    microgear.loop();
+//    Serial.println("connected");
+    
+//    String data = String(test_count);
+    char msg[128];
+//    data.toCharArray(msg, data.length());
+    int i = 0;
+    while(STMSerial.available() > 0)
+    {
+      msg[i++] = STMSerial.read();
+      delay(1);
+    }
+    msg[i] = '\0';
+    if(i > 0)
+    {
+      Serial.print("Message received: ");    
+      Serial.println(msg);
+      microgear.chat(TargetWeb, msg);
+    }
+  }
+  else 
+  {
+    Serial.println("connection lost, reconnect...");
+    microgear.connect(APPID);
+  }
+  delay(100);
+}
