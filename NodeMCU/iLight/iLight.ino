807faf4a0a8650a2b98a872b3a31d90edc47d577
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-12-20 12:02:00 +0700

    Supported multiple WiFi
    NodeMCU now waits for WiFi before it starts working (ensureWiFi())

--- NodeMCU/iLight/iLight.ino
+++ NodeMCU/iLight/iLight.ino
@@ -2,27 +2,36 @@
 
 #include <MicroGear.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 
 #define APPID   "iLight"
 #define KEY     "Q3XUy22q0cgxB9R"
 #define SECRET  "LgJmdco46P1GzPEslEhJBQ7il"
-
-const char* ssid     = "Aphires";
-const char* password = "AllHearsMe";
 
 #define ALIAS   "iLight_NodeMCU"
 #define TargetWeb "HTML_web_test"
 
 SoftwareSerial STMSerial(5, 4); // D1, D2
 
+ESP8266WiFiMulti wifiMulti;
+
 WiFiClient client;
 MicroGear microgear(client);
 
 int test_count;
 
+void addWiFi()
+{
+  wifiMulti.addAP("Aphires", "AllHearsMe");
+  wifiMulti.addAP("yong", "bcd28c3d35ff");
+//  wifiMulti.addAP("ssid", "password");
+//  wifiMulti.addAP("ssid", "password");
+//  wifiMulti.addAP("ssid", "password");
+}
+
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) 
 {
-  Serial.print("Incoming message --> ");
+  Serial.print("Message from NETPIE: ");
   msg[msglen] = '\0';
   Serial.println((char *) msg);
   STMSerial.print('[');
@@ -33,8 +42,26 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen)
 
 void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) 
 {
-  Serial.println("Connected to NETPIE...");
+  Serial.println("Connected to NETPIE.");
   microgear.setAlias(ALIAS);
+}
+
+void ensureWiFi()
+{
+  if (wifiMulti.run() != WL_CONNECTED)
+  {
+    Serial.print("Looking for WiFi");
+    while(wifiMulti.run() != WL_CONNECTED)
+    {
+      Serial.print(".");
+      delay(250);
+    }
+    Serial.println();
+    Serial.print("Connected to: ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
 }
 
 void setup() 
@@ -43,38 +70,26 @@ void setup()
   microgear.on(MESSAGE, onMsghandler);
   microgear.on(CONNECTED, onConnected);
   
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Starting...");
   
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(250);
-    Serial.print(".");
-  }
-  
-  Serial.print("Connected to WiFi: ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  STMSerial.begin(9600);
+
+  addWiFi();
+  ensureWiFi();
   
   microgear.init(KEY,SECRET,ALIAS);
   microgear.connect(APPID);
-  
-  test_count = 0;
-  STMSerial.begin(9600);
 }
 
 void loop() 
 {
+  ensureWiFi();
   if (microgear.connected())
   {
     microgear.loop();
 //    Serial.println("connected");
-    
-//    String data = String(test_count);
     char msg[128];
-//    data.toCharArray(msg, data.length());
     int i = 0;
     while(STMSerial.available() > 0)
     {
@@ -84,14 +99,14 @@ void loop()
     msg[i] = '\0';
     if(i > 0)
     {
-      Serial.print("Message received: ");    
+      Serial.print("Message from STM32: ");    
       Serial.println(msg);
       microgear.chat(TargetWeb, msg);
     }
   }
   else 
   {
-    Serial.println("connection lost, reconnect...");
+    Serial.println("Reconnecting to NETPIE...");
     microgear.connect(APPID);
   }
   delay(1000);
